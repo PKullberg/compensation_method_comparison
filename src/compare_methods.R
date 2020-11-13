2e646a921c53a1a6d50c235bf22a29c06e385931
Peter Kullberg <5092000+PKullberg@users.noreply.github.com>
2020-11-13 16:54:47 +0200

    Just some typos

--- src/compare_methods.R
+++ src/compare_methods.R
@@ -2,10 +2,10 @@
 # Author: Peter Kullberg, peter.kullberg@ymparisto.fi
 library(tidyverse)
 
-# Load the calclators non-scaled and scaled
+# Load the calculators
 source("src/calculators.R")
 
-# Create a value space taht covers all five variables evenly
+# Create a value space that covers all five variables evenly
 d1f <- seq(0, 1, by = 0.1)
 d2f <- seq(0, 1, by = 0.1)
 d3f <- seq(0, 1, by = 0.1)
@@ -22,12 +22,12 @@ df <- expand.grid(d1f, d2f, d3f, ltf, blf) %>% rename(d1f = Var1, d2f = Var2, d3
          bl = blf * 50)
 
 # compute the condition values
-# 1. elite_scaled = traditional elite aggregation stretched to range from 0 to 1 (interpertation of the value "proportion from minimum")
-# 2. hh = weighted aritmentic mean of all indices (similar is used in habitat hectares approach, thus the  hh)
+# 1. elite_scaled = traditional elite aggregation stretched to range from 0 to 1 (interpretation of the value: "proportion from minimum")
+# 2. hh = weighted arithmentic mean of all indices (similar is used in habitat hectares approach, thus the  hh)
 df_values <- df %>% rowwise() %>% mutate(elite_scaled = ELITE_value_scaled(c(d1, d2, d3, lt, bl), habitat = "Kangas_new"),
                                          hh = mean_value(c(d1, d2, d3, lt, bl), habitat = "Kangas_new"))
 
-# Scatter plot of the methods
+# Scatter plot of ecological values using different methods
 ggplot(df_values, aes(x = hh, y = elite_scaled)) + 
   geom_point(size = 1, alpha = 0.5) + 
   geom_abline(slope = 1, lty = 2) + 
@@ -35,7 +35,7 @@ ggplot(df_values, aes(x = hh, y = elite_scaled)) +
        y = 'Habitat condition computed with the multiplicative method')
 
 # test how different methods affect required compensation ratios
-# Draw random "sites to be developped" and then another to compensate it and compute requiret ofset ratio
+# Draw a random "sites to be developped" and another to act as a offset for it and compute requiret offset ratio
 samp1 <- sample(1:nrow(df_values), 50000)
 samp2 <- sample(1:nrow(df_values), 50000)
 random_comp <- cbind(x = df_values[samp1, ], y = df_values[samp2, ]) %>%
@@ -43,10 +43,9 @@ random_comp <- cbind(x = df_values[samp1, ], y = df_values[samp2, ]) %>%
          hh_dif = x.hh / y.hh)
 
 # This plot shows that, when the random site is compensated with one that is in better condition (compensation ration is lower than 1)
-# elite requires on average larger compensation ratios than hh.
+# elite method requires on average larger compensation ratios than hh.
 # If the random site is compensated with one that is in worse condition (compensation ration is higher than 1)
-# elite requires in general smaller compensation ratios than hh.
-
+# elite method requires in general smaller compensation ratios than hh.
 ggplot(random_comp, aes(x = hh_dif, y = elite_s_dif)) + 
   geom_point(size = 1.3, col = "black", alpha = 0.5) + 
   geom_smooth(col = "red") + 
